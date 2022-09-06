95173bf6daa2b0fd069bc1eedb8a9bb306aeec45
Raquel Joseph Abaya <rjabaya0110@gmail.com>
2022-09-06 19:24:30 -0400

    added my greeting

--- CodeLouisville_GitExercise/Program.cs
+++ CodeLouisville_GitExercise/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("Hello! I'm Michael");
 Console.WriteLine("Hello! I'm Isaac");
 Console.WriteLine("Hello! I'm Justin");
 Console.WriteLine("Hello! I'm Scott");
+Console.WriteLine("Hello! I'm Joseph");
 
 
 
